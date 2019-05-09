cf3d0648b44f900acf441fee5ed582ae5e8c002b
Ahmed Nouira <ahmnouira@gmail.com>
2019-05-09 03:39:35 +0100

    2a
        Hello World Flask
    
        Add Bootstrap Extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask 		#import Flask 
 from flask_bootstrap import Bootstrap	#import Bootstrap
 App = Flask(__name__)		#create Flask object
-boostrap = Bootstrap(App) 
+boostrap = Bootstrap(App)                             #create bootstrap object
 from app import routes 		#import routes
