957aba449092b00b7fd7a648c7f737e22726e606
AhmNouira <ahmnouira@gmail.com>
2019-07-16 21:34:18 +0100

    add gitignore

--- app/config.py
+++ app/config.py
@@ -3,4 +3,4 @@ import os
 
 class Config(object):
     PASSWORD = os.environ.get('PASSWORD') or 'ahmednouira'
-    MAX_ATTEMPTS = os.environ.get('MAX_ATTEMPTS') or 3
+    MAX_ATTEMPTS = os.environ.get('MAX_ATTEMPTS') or 2
